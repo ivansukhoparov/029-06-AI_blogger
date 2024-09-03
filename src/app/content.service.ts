cead550be9d1a50b95b06d7caff1d97d2b6ba132
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-03 02:29:00 +0200

    refactoring:  ContentItemType  has been extended with ContentItem class

--- src/app/content.service.ts
+++ src/app/content.service.ts
@@ -1,25 +1,33 @@
-import {ContentItem} from "./types/content";
+import {ContentItemType} from "./types/content";
 import {PromptsService} from "./prompts.service";
 import {ContentRepository} from "./content.repository";
 import {IGptService} from "./interfaces/gpt.servise.interface";
-import {inject} from "inversify";
+import {inject, injectable} from "inversify";
 import {GptService} from "./gpt.service";
+import {SchedulerService} from "../scheduler/scheduler.service";
+import {AppSettings} from "../settings/app.settings";
+import {ContentPlanParamsType, FREQUENCIES, TIMEFRAMES} from "./common/common";
 
+@injectable()
 export class ContentService {
 
     constructor(@inject(GptService) private gptService: IGptService,
                 @inject(PromptsService) private promptService: PromptsService,
-                @inject(ContentRepository) private contentRepository: ContentRepository) {
+                @inject(ContentRepository) private contentRepository: ContentRepository,
+                @inject(SchedulerService) private schedulerService:SchedulerService,
+                @inject(AppSettings) private appSettings: AppSettings) {
     }
 
-    async createContentPlan(startDate: string) {
-        const timeframe = "3 days"
-        const frequency = "daily"
 
-        const prompt = this.promptService.generateContentPlanPrompt(timeframe, frequency)
+    async createContentPlan(params: ContentPlanParamsType) {
+        const timeframe = "week"
+        const frequency = "daily"
+        const prompt = this.promptService.generateContentPlanPrompt(params)
         const rawContentPlan = await this.gptService.jsonRequest(prompt)
-        const contentPlanDto = this._contentPlanMapper(rawContentPlan.content_plan, startDate)
-        return await this.contentRepository.extendContentPlan(contentPlanDto)
+        const contentPlanDto = this._contentPlanMapper(rawContentPlan.content_plan, params)
+        const contentIds=  await this.contentRepository.extendContentPlan(contentPlanDto)
+
+
     }
 
     async updatePostWithContent(publicationDate: string) {
@@ -28,6 +36,18 @@ export class ContentService {
         const content = await this.gptService.jsonRequest(prompt)
         const updateDto = this._updateContentPlanItem(content)
         return await this.contentRepository.updateContentPlanItem(contentPlanItem.id, updateDto)
+    }
+
+
+    async createAdditionalPost(publicationDate: string) {
+        const postId = await this.contentRepository.addPost(this._post())
+        if (postId) {
+            const prompt = this.promptService.generateAdditionalPostPrompt()
+            const content = await this.gptService.jsonRequest(prompt)
+            const updateDto = this._updateContentPlanItem(content)
+            await this.contentRepository.updateContentPlanItem(postId, updateDto)
+            return content.content
+        }
     }
 
     _updateContentPlanItem(rawContent: any) {
@@ -44,16 +64,21 @@ export class ContentService {
         }
     }
 
-    _contentPlanMapper(rawContentPlan: any, startDate: string) {
-        const mappedContentPlan = []
-        const year = Number(startDate.split("-")[0])
-        const month = Number(startDate.split("-")[1]) - 1
-        const day = Number(startDate.split("-")[2])
 
-        let date = new Date(Date.UTC(year, month, day));
+    _contentPlanMapper(rawContentPlan: Array<any>, contentPlanParams:ContentPlanParamsType) {
+        const startDate = contentPlanParams.startDate
+        const dayOffset:number =  FREQUENCIES[contentPlanParams.frequency]
 
-        for (let item of rawContentPlan) {
-            const contentItem: ContentItem = {
+        const contentPlan: Array<any> = rawContentPlan.length= TIMEFRAMES[contentPlanParams.timeframe]
+        const mappedContentPlan: Array<any>  = []
+        const year:number = Number(startDate.split("-")[0])
+        const month:number = Number(startDate.split("-")[1]) - 1
+        const day:number = Number(startDate.split("-")[2])
+
+        let date:Date = new Date(Date.UTC(year, month, day));
+
+        for (let item of contentPlan) {
+            const contentItem: ContentItemType = {
                 createdAt: new Date().toISOString(),
                 updatedAt: null,
                 isPublicized: false,
@@ -75,10 +100,40 @@ export class ContentService {
                 image: null,
                 image_prompt: null
             }
-            date = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1));
+            date = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + dayOffset));
             mappedContentPlan.push(contentItem)
         }
         return mappedContentPlan
+    }
+
+    _post() {
+
+        let date = new Date();
+
+        const contentItem: ContentItemType = {
+            createdAt: new Date().toISOString(),
+            updatedAt: null,
+            isPublicized: false,
+            isGenerated: false,
+            type: "main",
+            publicationDate: date.toISOString().split("T")[0],
+            topic: "item.topic",
+            title: "item.title",
+            outline: "item.outline",
+            keywords: ["item.keywords"],
+            contentType: "item.content_type",
+            shortDescription: null,
+            shortContentRu: null,
+            shortContentEn: null,
+            fullContentRu: null,
+            fullContentEn: null,
+            additionalKeyword: null,
+            with_image: null,
+            image: null,
+            image_prompt: null
+        }
+
+        return contentItem
     }
 
 }
