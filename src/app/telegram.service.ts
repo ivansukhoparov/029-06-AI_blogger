9038ece1c143a194dc8f90f5af64c8804b942199
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-04 02:31:03 +0200

    add:  The TelegramService  has been added

---
+++ src/app/telegram.service.ts
@@ -0,0 +1,11 @@
+import {injectable} from "inversify";
+
+@injectable()
+export class TelegramService {
+    constructor() {
+    }
+
+    publicatePost(content: string) {
+        return true
+    }
+}
