8981eea7df42a0fada7dd28796e5d00eef1e41cd
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-07 21:23:03 +0200

    refactoring: test method that check connection and switch it to proxy if connection has been blocked

--- src/app/gpt.service.ts
+++ src/app/gpt.service.ts
@@ -1,13 +1,16 @@
 import OpenAI from "openai";
 import {writeDownToFile} from "../utils/fs";
 import {IGptService} from "./interfaces/gpt.servise.interface";
-import {injectable} from "inversify";
-
+import {inject, injectable} from "inversify";
+import {SocksProxyAgent} from 'socks-proxy-agent';
+import {TelegramAdapter} from "./adapters/telegram.adapter";
 
 @injectable()
 export class GptService implements IGptService {
-    private openai:OpenAI
-    constructor() {
+    private useProxy:boolean = false
+    public openai: OpenAI
+
+    constructor(@inject(TelegramAdapter) private telegramAdapter:TelegramAdapter) {
         this.openai = new OpenAI();
     }
 
@@ -16,7 +19,8 @@ export class GptService implements IGptService {
             const time = new Date().toISOString().replaceAll(":", ".")
 
             const requestType = this._getRequestTypeFromPrompt(promptMessage)
-            writeDownToFile(promptMessage, `${time}--1-${requestType}_request.json`)
+            writeDownToFile(promptMessage, `${time}--1-${requestType}_request.json`
+            )
 
             const response = await this.openai.chat.completions.create({
                 model: "gpt-4o",
@@ -44,7 +48,8 @@ export class GptService implements IGptService {
             writeDownToFile(json, `${time}--3-${requestType}_parsed_response.json`)
 
             return json;
-        } catch (err) {
+        } catch
+            (err) {
             console.log(err)
         }
     }
@@ -55,6 +60,33 @@ export class GptService implements IGptService {
             return requestType
         } else {
             return "other"
+        }
+    }
+
+    async test() {
+        try {
+            const response = await this.openai.chat.completions.create({
+                model: "gpt-4o",
+                messages: [{role: "system", content: `say "OK"`}],
+                max_tokens: 5,
+            });
+            if (response.choices[0].message.content === "OK") {
+                this.telegramAdapter.serviceMsg("gpt service started")
+                return true
+            }
+        } catch (err) {
+            console.log(err)
+            if (this.useProxy === false){
+                const agent = new SocksProxyAgent('socks5h://127.0.0.1:1080');
+                this.openai = new OpenAI({
+                    httpAgent: agent,
+                });
+                this.useProxy = true
+                return await this.test()
+            }else   {
+                this.telegramAdapter.serviceMsg("gpt service failed")
+                return false
+            }
         }
     }
 }
