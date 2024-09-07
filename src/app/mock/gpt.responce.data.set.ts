d5abf228d4b048ac7741527f11afda7114ffb236
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-07 21:14:31 +0200

    tests

---
+++ src/app/mock/gpt.responce.data.set.ts
@@ -0,0 +1,89 @@
+export const jsonContentPlanItemTestData: { content_plan: Array<unknown> } =
+    {
+        "content_plan": [
+            {
+                "date": "Monday",
+                "topic": "topic 1",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-11", "word-12"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Tuesday",
+                "topic": "topic 2",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-22", "word-23"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Wednesday",
+                "topic": "topic 3",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-31", "word-33"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Thursday",
+                "topic": "topic 4",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-44", "word-43"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Friday",
+                "topic": "topic 5",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-55", "word-56"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Saturday",
+                "topic": "topic 6",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-67", "word-65"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            },
+            {
+                "date": "Sunday",
+                "topic": "topic 7",
+                "title": "post title",
+                "outline": "some outlines",
+                "keywords": ["word-1", "word-2", "word-71", "word-72"],
+                "content_type": "content type",
+                "post_length": "1200-1500 words",
+                "language": "ru"
+            }
+        ]
+    }
+
+export const jsonPostContentTestData = {
+    "content": "content content",
+    "keywords": [
+        "keywords2",
+        "keywords1",
+        "keywords3",
+        "keywords4",
+        "keywords5"
+    ],
+    "description": "content description",
+    "image_prompt": "image prompt",
+    "with_image": true,
+}
---
+++ src/app/mock/gpt.service.mock.ts
@@ -0,0 +1,37 @@
+import OpenAI from "openai";
+import "dotenv"
+import {config} from "dotenv";
+import {jsonContentPlanItemTestData, jsonPostContentTestData} from "./gpt.responce.data.set";
+import {writeDownToFile} from "../../utils/fs";
+import {IGptService} from "../interfaces/gpt.servise.interface";
+import {injectable} from "inversify";
+
+config()
+
+const openai = new OpenAI();
+
+@injectable()
+export class GptServiceMock implements  IGptService{
+    constructor() {
+    }
+    async jsonRequest(promptMessage: any):Promise<any> {
+        const requestType = this._getRequestTypeFromPrompt(promptMessage)
+        if (requestType === "get_content_plan"){
+            return jsonContentPlanItemTestData
+        }else if(requestType === "generate_post"){
+            return jsonPostContentTestData
+        }else{
+            return null
+        }
+
+    }
+
+    private _getRequestTypeFromPrompt(prompt: any): string {
+        let requestType = JSON.stringify(prompt).split(`\\"request_type\\":`)[1].split(`\\"`)[1]
+        if (requestType === "get_content_plan" || requestType === "generate_post") {
+            return requestType
+        } else {
+            return "other"
+        }
+    }
+}
