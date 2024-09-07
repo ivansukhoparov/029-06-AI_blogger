cc45b8c73d1ccf96d76cc4142672c98863709d64
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-07 21:27:30 +0200

    add: the method _setTaskTime has been added

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -2,23 +2,36 @@ import {inject, injectable} from "inversify";
 import {TaskBaseType} from "./types/tasks";
 import {SchedulerRepository} from "./scheduler.repository";
 
+export type SetTaskTimeParamType = {
+    date: Date,
+    hours?: number
+    minutes?: number
+}
 
 @injectable()
 export class SchedulerService {
     constructor(@inject(SchedulerRepository) private scheduleRepository: SchedulerRepository) {
     }
 
-
-    async createTask(command: string, time: Date, args: any) {
+    async createTask(command: string, time: SetTaskTimeParamType, args: any, description: string) {
         const createTaskDto: TaskBaseType = {
             commandName: command,
-            executionTime: time.toISOString(),
+            executionTime: this._setTaskTime(time).toISOString(),
             args: args,
             status: "pending",
+            description: description
         }
         return await this.scheduleRepository.createTask(createTaskDto)
-
     }
 
-
+    private _setTaskTime(params: SetTaskTimeParamType) {
+        const newDate = new Date(params.date)
+        if (params.hours) {
+            newDate.setHours(newDate.getHours() + params.hours)
+        }
+        if (params.minutes) {
+            newDate.setMinutes(newDate.getMinutes() + params.minutes)
+        }
+        return newDate
+    }
 }
