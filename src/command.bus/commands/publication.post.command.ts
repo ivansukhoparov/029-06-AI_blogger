f78428d016e652f9e99dbcff5b7d7eb257cf268d
ivansukhoparov <rbcdaaa@gmail.com>
2024-09-08 13:41:08 +0200

    refactoring: eslint and prettier refactoring

--- src/command.bus/commands/publication.post.command.ts
+++ src/command.bus/commands/publication.post.command.ts
@@ -1,26 +1,24 @@
-import {inject, injectable} from "inversify";
-import {ICommand} from "../interface.command";
-import {ContentRepository} from "../../app/content.repository";
-import {PublicationPostCommandType, UpdatePostContentCommandParamType} from "../../app/common/common";
-import {ContentItem} from "../../app/types/content";
-import {TelegramService} from "../../app/telegram.service";
-import {TelegramAdapter} from "../../app/adapters/telegram.adapter";
-
+import { inject, injectable } from 'inversify';
+import { ICommand } from '../interface.command';
+import { ContentRepository } from '../../app/content.repository';
+import { PublicationPostCommandType, UpdatePostContentCommandParamType } from '../../app/common/common';
+import { ContentItem } from '../../app/types/content';
+import { TelegramService } from '../../app/telegram.service';
+import { TelegramAdapter } from '../../app/adapters/telegram.adapter';
 
 @injectable()
 export class PublicationPostCommand implements ICommand {
-    name = "PublishPostCommand"
+  name = 'PublishPostCommand';
 
-    constructor(@inject(ContentRepository) private contentRepository: ContentRepository,
-                @inject(TelegramAdapter) private telegramAdapter:TelegramAdapter
-                ) {
-    }
+  constructor(
+    @inject(ContentRepository) private contentRepository: ContentRepository,
+    @inject(TelegramAdapter) private telegramAdapter: TelegramAdapter,
+  ) {}
 
-    async execute(params: PublicationPostCommandType): Promise<boolean> {
-        const post:ContentItem = await this.contentRepository.getContentPlanItem({id:params.postId})
-        const textContent = post.textContent
-        await this.telegramAdapter.sendMessage(Number(params.target),textContent)
-        return true
-    }
-
+  async execute(params: PublicationPostCommandType): Promise<boolean> {
+    const post: ContentItem = await this.contentRepository.getContentPlanItem({ id: params.postId });
+    const textContent = post.textContent;
+    await this.telegramAdapter.sendMessage(Number(params.target), textContent);
+    return true;
+  }
 }
